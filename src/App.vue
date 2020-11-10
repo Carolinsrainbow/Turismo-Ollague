0c9eddd1f57d55e023863ffc4076fe53d4d27fbd
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2020-11-09 21:58:05 -0300

    Incorporación Reserva

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/artesania">Artesania</router-link>
-    </div>
+
     <router-view/>
   </div>
 </template>
+
+
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+     name: "Artesania"
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,17 @@
 <template>
 <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#"></b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <b-nav-item href="#">Circuito Turistico</b-nav-item>
+        <b-nav-item href="#">Galería</b-nav-item>
+  <b-nav-item href="#">Circuitos</b-nav-item>
+    <b-nav-item href="#">Contacto</b-nav-item>
+      <b-nav-item href="#">Inicio</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
@@ -18,21 +21,6 @@
           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
         </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
