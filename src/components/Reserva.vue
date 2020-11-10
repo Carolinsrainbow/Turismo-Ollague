3c0b94c8e38c14e29f97472f9f480a10be12224f
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2020-11-09 22:12:16 -0300

    .

--- src/components/Reserva.vue
+++ src/components/Reserva.vue
@@ -1,5 +1,28 @@
 <template>
     <div>
-        
+        <h1>RESERVA</h1>
+<div class="container">
+      <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+  </div>
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+  </div>
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+  </div>
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+  </div>
+
+  <button type="button" class="btn btn-warning">Reserva</button>
+</div>
+
     </div>
 </template>
+
+<script>
+export default {
+    name:"Reserva"
+}
+</script>
--- src/views/Artesania.vue
+++ src/views/Artesania.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
    <Navbar></Navbar>
+   <Reserva></Reserva>
     </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Reserva from  '@/components/Reserva.vue'
 export default {
     name: "Artesania"
 }
