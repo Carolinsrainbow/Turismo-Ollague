a0ad2e5e9e17ff880199da0ad92d8d4509ef9acc
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2020-11-26 15:39:11 -0300

    ,

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Artesania from '../views/Home.vue'
+import Artesania from  '../views/Artesania.vue'
 
 Vue.use(VueRouter)
 
